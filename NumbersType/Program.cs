ffe392c511244c9a463941f6a2c781aff3e206bf
Harsh <hp7075161@gmail.com>
2022-01-24 14:26:28 +0530

    Final Version

---
+++ NumbersType/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using NumbersApp;
+
+namespace Program
+{
+    class MainProgram{
+        public static void Main(string []args)
+        {
+            NumbersApp.Numbers.IntDemo();
+            NumbersApp.Numbers.DoubleDemo();
+            NumbersApp.Numbers.DecimalDemo();
+            NumbersApp.Numbers.findArea();
+        }
+    }
+}
+
